16d0d02eba48b380f4cd053ce630539582a2bd85
goobneighbor <goobneighbor@naver.com>
2023-06-28 17:44:11 +0900

    Main2903

---
+++ Java01_main/src/Main2903.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2903 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		int result = (int)Math.pow((Math.pow(2, N)+1), 2); 
+		
+		System.out.println(result);
+	}
+}
