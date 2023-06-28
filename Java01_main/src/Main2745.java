d3cb7e1f79b98c5a42885e5d210ad5f7f5636e0f
goobneighbor <goobneighbor@naver.com>
2023-06-28 15:00:38 +0900

    Main2745_2nd

--- Java01_main/src/Main2745.java
+++ Java01_main/src/Main2745.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main2745 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -14,41 +14,20 @@ public class Main2745 {
 		String N = st.nextToken();
 		int B = Integer.parseInt(st.nextToken());
 		
-		char[] wrd = new char[N];
+		int result=0;
+		int sum=0;
 		
-		for(int i=0; i<wrd.length; i++) {
-			char[] wrd = N.charAt(i);
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		
-		int i = 0;
-		while() {
-			if(N.charAt(i)>=48 && N.charAt(i)<=57) {
-				sb.append((N.charAt(i)-48) * Math.pow(B, B))
-			}else if(N.charAt(i)>=65 && N.charAt(i)<=90) {
-				
+		for(int i=0; i<N.length(); i++) {
+			char c = N.charAt(i);
+			
+			if('0'<=c && c<='9') {
+				result=c-48;
+			}else if('A'<=c && c<='Z'){
+				result=c-55;
 			}
+			
+			sum+= result * Math.pow(B, N.length()-1-i);
 		}
-		
-		int i=0;
-		
-		
-		
-		if((char)Integer.parseInt(wrd[i])>=48 && (char)Integer.parseInt(wrd[i])<=57) {
-			sb.append(Integer.parseInt(wrd[i]) * Math.pow(B, wrd.length-1-i));
-		}else if((char)Integer.parseInt(wrd[i])>=65 && (char)Integer.parseInt(wrd[i])<=90) {
-			sb.append(wrd[i] * (byte)Math.pow(B, wrd.length-1-i));
-		}
-		
-		int sum = 0;
-		
-		for(byte k : wrd) {
-			sum+=k;
-		}
-		
 		System.out.println(sum);
-		
-		
 	}
 }
